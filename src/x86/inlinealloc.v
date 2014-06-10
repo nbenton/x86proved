2b992b63bae96182b1cd27d926245a7306d8ce71
Jason Gross <t-jagro@microsoft.com>
2014-06-10 16:43:25 +0100

    Minor cleanup in inlinealloc
    After    | File Name       | Before   || Change
    --------------------------------------------------
    1m34.37s | Total           | 1m26.04s || +0m08.32s
    --------------------------------------------------
    0m34.17s | x86/listproof   | 0m30.95s || +0m03.22s
    0m21.67s | x86/wrapalloc   | 0m18.70s || +0m02.97s
    0m24.64s | x86/inlinealloc | 0m23.90s || +0m00.74s
    0m06.96s | x86/listspec    | 0m06.44s || +0m00.51s
    0m06.94s | x86/listimp     | 0m06.06s || +0m00.88s

--- src/x86/inlinealloc.v
+++ src/x86/inlinealloc.v
@@ -56,7 +56,7 @@ Corollary ADD_RI_ruleAux (r:Reg) v1 (v2:DWORD):
             (let v := addB v1 v2 in
              r~=v ** OSZCP (computeOverflow v1 v2 v) (msb v)
                             (v == #0) (fst (adcB false v1 v2)) (lsb v)).
-Proof. unfold addB. apply ADD_RI_rule. Qed. 
+Proof. unfold addB. apply ADD_RI_rule. Qed.
 
 Lemma inlineAlloc_correct n failed infoBlock : |-- allocSpec n failed (allocInv infoBlock) (allocImp infoBlock n failed).
 Proof.
@@ -74,35 +74,34 @@ Proof.
   specapply ADD_RI_ruleAux; first by ssimpl.
 
   (* JC failed *)
-  specapply JC_rule; first by rewrite /OSZCP; sbazooka.
+  specapply JC_rule; first by rewrite /OSZCP; ssimpl.
   repeat specsplit.
   { rewrite <-spec_reads_frame. rewrite <-spec_later_weaken.
     autorewrite with push_at. apply limplValid. apply landL1. cancel1.
     rewrite /stateIsAny /allocInv. by sbazooka. }
 
   (* CMP [ESI+4], EDI *)
-  specintro. move/eqP => Hcarry. (*rewrite Hcarry.*)
+  specintro. move/eqP => Hcarry.
   specapply CMP_MR_ZC_rule; first by rewrite /stateIsAny; sbazooka.
 
   (* JC failed *)
   specapply JC_rule; first by ssimpl.
 
-  specsplit.
+  repeat specsplit.
   { rewrite <-spec_reads_frame. rewrite <-spec_later_weaken.
     autorewrite with push_at. apply limplValid. apply landL1. cancel1.
     rewrite /stateIsAny/allocInv. sbazooka. }
 
   (* MOV [ESI], EDI *)
-  specintro => LT. 
-  specapply (MOV_M0R_rule (pd:=infoBlock) (r1:=ESI)); first by sbazooka.
-  rewrite <-spec_reads_frame. apply limplValid. autorewrite with push_at.
-  apply: landL2. cancel1.
-  rewrite /allocInv. ssplits.
-
-  rewrite /stateIsAny/natAsDWORD. sbazooka. 
-  apply memAnySplit. 
-  apply: addB_leB. apply injective_projections. by rewrite Hcarry. 
-  unfold addB. by generalize @adcB. 
-
-  simpl. rewrite ltBNle eqb_negLR /negb /natAsDWORD in LT. by rewrite (eqP LT). 
+  specintro => LT.
+  specapply MOV_M0R_rule; first by ssimpl.
+  { rewrite <-spec_reads_frame. apply limplValid. autorewrite with push_at.
+    apply: landL2. cancel1.
+    rewrite /allocInv. ssplits.
+    rewrite /stateIsAny/natAsDWORD. sbazooka.
+    apply memAnySplit.
+    { apply: addB_leB. rewrite /addB.
+      apply injective_projections; [ by rewrite Hcarry
+                                   | by generalize @adcB ]. }
+    { simpl. rewrite ltBNle eqb_negLR /negb /natAsDWORD in LT. by rewrite (eqP LT). } }
 Qed.
