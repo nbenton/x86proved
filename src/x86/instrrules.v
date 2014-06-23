487d9d876b3650595e565f7f3dfd2712c9a4c6f7
Jason Gross <t-jagro@microsoft.com>
2014-06-23 20:26:15 +0100

    Remove useless imports from instrrules.v

--- src/x86/instrrules.v
+++ src/x86/instrrules.v
@@ -1,22 +1,6 @@
-Ltac type_of t := type of t (* ssreflect bug workaround *).
 (*===========================================================================
     Rules for x86 instructions in terms of [safe]
   ===========================================================================*)
-Require Import ssreflect ssrbool ssrnat ssrfun eqtype seq fintype tuple.
-Require Import procstate procstatemonad bitsops bitsprops bitsopsprops.
-Require Import spec SPred septac spec safe triple basic basicprog spectac.
-Require Import instr instrcodec eval monad monadinst reader pointsto cursor.
-Require Import Setoid RelationClasses Morphisms.
-
-Set Implicit Arguments.
-Unset Strict Implicit.
-Import Prenex Implicits.
-
-Require Import Relations.
-Require Import instrsyntax.
-
-Local Open Scope instr_scope.
-
 Require Export x86.instrrules.core.
 
 Require Export x86.instrrules.adc.
