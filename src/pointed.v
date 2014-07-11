503ce30f15bab6ba2abd252c14585ea1721799be
Jason Gross <t-jagro@microsoft.com>
2014-07-11 13:46:00 +0100

    Add some pointed instances

--- src/pointed.v
+++ src/pointed.v
@@ -3,6 +3,7 @@
     The "pointed" terminology comes from the homotopy type theoretic
     practice of thinking of types as ∞-groupoids or topological
     spaces. *)
+Require Import Ssreflect.ssreflect.
 Require Import Coq.Setoids.Setoid Coq.Classes.RelationClasses.
 
 Set Implicit Arguments.
@@ -25,10 +26,14 @@ Instance IsPointed_sig2 {A} {P Q : A -> Prop} `{IsPointed A, IsPointed (P (point
 Instance IsPointed_ex2 {A} {P Q : A -> Prop} `{IsPointed A, IsPointed (P (point A)), IsPointed (Q (point A))} : IsPointed (ex2 P Q)
   := ex_intro2 P Q (point A) (point _) (point _).
 Instance IsPointed_forall {A B} `{forall a : A, IsPointed (B a)} : IsPointed (forall a : A, B a) := fun a => point (B a).
+Instance IsPointed_refl {T a} : IsPointed (a = a :> T) := eq_refl.
+Instance IsPointed_reflexive `{@Reflexive T R} {a} : IsPointed (R a a) := reflexivity _.
+
+(** TODO(t-jagro): Should this be an instance? *)
+Instance IsPointed_list {A} : IsPointed (list A) := nil.
+
 (** We don't make the [or] versions instances, because they're not unique *)
 Definition IsPointed_sum_inl {A B} `{IsPointed A} : IsPointed (A + B) := inl (point A).
 Definition IsPointed_sum_inr {A B} `{IsPointed B} : IsPointed (A + B) := inr (point B).
 Definition IsPointed_or_introl {A B : Prop} `{IsPointed A} : IsPointed (A \/ B) := or_introl (point A).
 Definition IsPointed_or_intror {A B : Prop} `{IsPointed B} : IsPointed (A \/ B) := or_intror (point B).
-Instance IsPointed_refl {T a} : IsPointed (a = a :> T) := eq_refl.
-Instance IsPointed_reflexive `{@Reflexive T R} {a} : IsPointed (R a a) := reflexivity _.
