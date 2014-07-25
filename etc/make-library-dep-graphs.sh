4044383a906d84940378461dd04e9a88366881e2
Jason Gross <t-jagro@microsoft.com>
2014-07-25 23:59:12 +0100

    Add a comment about colors

--- etc/make-library-dep-graphs.sh
+++ etc/make-library-dep-graphs.sh
@@ -8,7 +8,13 @@ cd "$DIR/../src"
 if [ "$SELF" -nt "$DIR/../src/Makefile.librarydepgraphs" ] || [ "$DIR"/coq-scripts/depgraphs/library/make-makefile.sh -nt "$DIR/../src/Makefile.librarydepgraphs" ]
 then
     echo "Making makefile"
-    bash "$DIR"/coq-scripts/depgraphs/library/make-makefile.sh ../src/Makefile.librarydepgraphs -t x86proved -c x86/=Magenta -c charge/=BlueViolet -c examples/=Orange -c tools/=Cyan4 -R x86proved=.
+    # colors for directories come from the list at http://hackage.haskell.org/package/graphviz-2999.13.0.3/docs/Data-GraphViz-Attributes-Colors-X11.html#t:X11Color
+    bash "$DIR"/coq-scripts/depgraphs/library/make-makefile.sh ../src/Makefile.librarydepgraphs -t x86proved -R x86proved=. \
+	-c x86/=Magenta \
+	-c charge/=BlueViolet \
+	-c examples/=Orange \
+	-c tools/=Cyan4
+
     rm -f "$DIR/../src/library.svg" "$DIR/../src/library.dot"
 fi
 
