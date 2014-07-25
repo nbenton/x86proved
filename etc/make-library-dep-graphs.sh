7c1e76604a6cc42aabec854ed6fb9b162878b65e
Jason Gross <t-jagro@microsoft.com>
2014-07-25 23:53:29 +0100

    Update the dep graph maker to do colors

--- etc/make-library-dep-graphs.sh
+++ etc/make-library-dep-graphs.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SELF="$(readlink -f "${BASH_SOURCE[0]}")"
 
 cd "$DIR/../src"
 
 # update makefile if we're newer
-# FIXME: this test doesn't work super-well
-if [ "${BASH_SOURCE[0]}" -nt "$DIR/../src/Makefile.librarydepgraphs" ] || [ "$DIR"/coq-scripts/depgraphs/library/make-makefile.sh -nt "$DIR/../src/Makefile.librarydepgraphs" ]
+if [ "$SELF" -nt "$DIR/../src/Makefile.librarydepgraphs" ] || [ "$DIR"/coq-scripts/depgraphs/library/make-makefile.sh -nt "$DIR/../src/Makefile.librarydepgraphs" ]
 then
     echo "Making makefile"
-    . "$DIR"/coq-scripts/depgraphs/library/make-makefile.sh ../src/Makefile.librarydepgraphs -t x86proved -c src/x86/=Magenta -c src/charge/=BlueViolet -c src/examples/=Orange -c src/tools/=Cyan4 -R x86proved=src/
+    bash "$DIR"/coq-scripts/depgraphs/library/make-makefile.sh ../src/Makefile.librarydepgraphs -t x86proved -c x86/=Magenta -c charge/=BlueViolet -c examples/=Orange -c tools/=Cyan4 -R x86proved=.
     rm -f "$DIR/../src/library.svg" "$DIR/../src/library.dot"
 fi
 
