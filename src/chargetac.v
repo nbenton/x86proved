ddef882bf4ac42f9ded312a6fd672cb93f2e6e77
Jason Gross <t-jagro@microsoft.com>
2014-08-06 14:43:55 +0100

    Add some tactics to chargetac
    We now have equivalents of intro, assumption, split, move at top, move at
    bottom, and hyp_apply *.  Also, improve the power of the general tactics.

--- src/chargetac.v
+++ src/chargetac.v
@@ -11,6 +11,96 @@ Unset Strict Implicit.
 Import Prenex Implicits.
 Generalizable All Variables.
 
+(** Like [split], but for ilogic *)
+Ltac ilsplit :=
+  (** First match the goal with the appropriate form, to get nice error messages *)
+  match goal with
+    | [ |- ?A |-- ?B //\\ ?C ] => idtac
+    | [ |- ?A |-- ?B ] => fail 0 "Conclusion" B "is not of the form _ //\\ _"
+    | [ |- ?G ] => fail 0 "Goal" G " is not of the form _ |-- _ //\\ _"
+  end;
+  apply landR.
+
+(** Like [assumption], except for ilogic, and use the given [unify] tactic for finding the right hypothesis *)
+Ltac ilassumption_with unify :=
+  (** Make this a proper tactic, not something returning a tactic (the [match] should not be immediate *)
+  idtac;
+  match goal with
+    | [ |- ?A |-- ?B ] => unify A B; reflexivity
+    | [ |- ?A //\\ ?B |-- ?C ] => apply landL1; ilassumption_with unify
+    | [ |- ?A //\\ ?B |-- ?C ] => apply landL2; ilassumption_with unify
+    | [ |- ?A |-- ?B ] => fail 1 "No assumption matching" B "in" A
+    | [ |- ?G ] => fail 1 "Goal" G "is not of the form ? |-- ?"
+  end.
+
+Ltac ilassumption := ilassumption_with ltac:(fun x y => unify x y).
+
+(** Like [intro] but for ilogic *)
+Ltac ilintro :=
+  match goal with
+    | [ |- ?A |-- ?B -->> ?C ] => idtac
+    | [ |- ?A |-- ?B ] => fail 0 "Conclusion" B "is not of the form _ -->> _"
+    | [ |- ?G ] => fail 0 "Goal" G " is not of the form _ |-- _ -->> _"
+  end;
+  apply limplAdj.
+
+Ltac ilintros := do ?ilintro.
+
+(** Look for an implication in the hypothesis list of an ilogic term [G] whose conclusion matches [concl] *)
+Ltac general_find_impl_with_concl G concl :=
+  match G with
+    | concl => constr:(G)
+    | ?A -->> ?B => let test := general_find_impl_with_concl B concl in constr:(G)
+    | ?A //\\ ?B => general_find_impl_with_concl A concl
+    | ?A //\\ ?B => general_find_impl_with_concl B concl
+    | _ => fail 1 "Statement" G "does not contain an implication ending with" concl
+  end.
+(** Look for an implication in the hypothesis list of an ilogic term [G] whose conclusion matches [concl], and which only has one assumption *)
+Ltac find_impl_with_concl G concl :=
+  match G with
+    | ?A -->> concl => constr:(G)
+    | ?A //\\ ?B => find_impl_with_concl A concl
+    | ?A //\\ ?B => find_impl_with_concl B concl
+    | _ => fail 1 "Statement" G "does not contain an implication of the form _ -->>" concl
+  end.
+(** Find an implication in the hypothesis list that can be applied *)
+Ltac find_impl_to_apply :=
+  let hyps := match goal with |- ?hyps |-- ?concl => constr:(hyps) end in
+  let concl := match goal with |- ?hyps |-- ?concl => constr:(concl) end in
+  find_impl_with_concl hyps concl.
+
+(** like [move hyp at top] and [move hyp at bottom], except matches on
+    type rather than name and is for ilogic *)
+Tactic Notation "ilmove" constr(hyp) "at" "bottom" :=
+  do ?[ rewrite -> !(landC hyp) | rewrite -> !landA ].
+Tactic Notation "ilmove" constr(hyp) "at" "top" :=
+  do ?[ rewrite <- !(landC hyp) | rewrite <- !landA ].
+
+(** Like [hyp_apply *], but it clears the hypothesis it applies, and
+    is for ilogic.  Used to progress on goals like [... //\\ (A -->>
+    B) //\\ .. |-- B] *)
+Ltac ilhyp_consume_apply :=
+  let impl := find_impl_to_apply in
+  ilmove impl at top;
+    rewrite -> ?landA;
+    match goal with
+      | [ |- ?A |-- ?A ] => reflexivity
+      | [ |- ?A -->> ?C |-- _ ] => rewrite -> (@landR _ _ _ (A -->> C) ltrue _ (reflexivity _) (ltrueR _))
+      | _ => idtac
+    end;
+    apply limplL; last by ilassumption.
+
+Lemma strip_andL_impl {Frm ILOps'} `{@ILogic Frm ILOps'}
+      {A B C B' C'}
+: @lentails Frm ILOps' (B -->> C) (B' -->> C')
+  -> @lentails Frm ILOps' (A //\\ B -->> C) (A //\\ B' -->> C').
+Proof.
+  move => H'.
+  apply landAdj in H'.
+  rewrite <- H'; clear H'.
+  do ![ ilsplit | ilassumption | ilintro | ilhyp_consume_apply ].
+Qed.
+
 Ltac simpl_logic' :=
   idtac;
   match goal with
@@ -53,7 +143,12 @@ Ltac finish_logic_with' tac :=
     | _ => progress tac
     | _ => syntax_unify_reflexivity
   end.
-Ltac finish_logic_with tac := do ?finish_logic_with' tac.
+Ltac finish_logic_with tac := do ?[ finish_logic_with' tac
+                                  | idtac;
+                                    match goal with
+                                      | [ |- _ //\\ _ |-- _ ] => apply landL1; by finish_logic_with tac
+                                      | [ |- _ //\\ _ |-- _ ] => apply landL2; by finish_logic_with tac
+                                    end ].
 Ltac finish_logic' := finish_logic_with' fail.
 Ltac finish_logic := finish_logic_with fail.
 
@@ -68,10 +163,20 @@ Proof.
          end.
 Qed.
 
+Lemma triple_limpl' `{@ILogic Frm ILOps'} (A B C : Frm) :  A -->> B -->> C |-- ((A -->> B) -->> A -->> C).
+Proof.
+  repeat match goal with
+           | _ => progress simpl_logic
+           | [ |- (?A //\\ ?B) //\\ ?C |-- _ ] => rewrite (landA A B C)
+           | [ |- (_ -->> _) //\\ _ |-- _ ] => eapply limplL; first by simpl_logic
+           | [ |- ?A //\\ (?B //\\ ?C) |-- _ ] => rewrite <- (landA A B C), (landC A B), -> (landA B A C); eapply limplL; first by simpl_logic
+         end.
+Qed.
+
 Ltac lrevert x :=
   revert x;
-  let T := match goal with |- forall H0 : ?T, |-- @?P H0 => constr:(T) end in
-  let P := match goal with |- forall H0 : ?T, |-- @?P H0 => constr:(P) end in
+  let T := match goal with |- forall H0 : ?T, _ |-- @?P H0 => constr:(T) end in
+  let P := match goal with |- forall H0 : ?T, _ |-- @?P H0 => constr:(P) end in
   intro x;
     setoid_rewrite <- (@lforallL _ _ _ T x P _ (reflexivity _));
     try clear x.
