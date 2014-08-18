69a6f080c8a004c09a527559c2faf1a2c58a31d1
Jason Gross <t-jagro@microsoft.com>
2014-08-18 19:58:54 +0100

    Add another import

--- src/x86/macros.v
+++ src/x86/macros.v
@@ -2,6 +2,7 @@ Require Import Ssreflect.ssreflect Ssreflect.ssrbool Ssreflect.ssrnat Ssreflect.
 Require Import x86proved.x86.procstate x86proved.x86.procstatemonad x86proved.bitsops x86proved.bitsprops x86proved.bitsopsprops.
 Require Import x86proved.spred x86proved.opred x86proved.septac x86proved.spec x86proved.obs x86proved.x86.basic x86proved.x86.program x86proved.spectac.
 Require Import x86proved.x86.instr x86proved.x86.instrsyntax x86proved.x86.instrcodec x86proved.x86.instrrules x86proved.reader x86proved.pointsto x86proved.cursor.
+Require Import x86proved.x86.basicprog (* for [instrrule] *).
 Require Import x86proved.basicspectac x86proved.common_tactics x86proved.common_definitions x86proved.chargetac.
 Require Import Coq.Classes.RelationClasses.
 
