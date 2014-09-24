9100852198e68dc11590b5e11c6204717932fb6b
andrewjkennedy <akenn@microsoft.com>
2014-09-24 22:35:44 +0100

    Generic or and xor rules

--- src/x86/instrrules/or.v
+++ src/x86/instrrules/or.v
@@ -2,20 +2,17 @@
 Require Import x86proved.x86.instrrules.core.
 Import x86.instrrules.core.instrruleconfig.
 
-Lemma OR_RM_rule s (pd:DWORD) (r1:VReg s) (r2:Reg) (v1 v2:VWORD s) (offset:nat) (v:VWORD s) :
-  orB v1 v2 = v ->
-  |-- basic (r1 ~= v1 ** r2 ~= pd ** pd +# offset :-> v2 ** OSZCP?)
-            (OR r1, [r2 + offset]) empOP
-            (r1 ~= v ** r2 ~= pd ** pd +# offset :-> v2 **
-             OSZCP false (msb v) (v == #0) false (lsb v)).
-Proof. move => ?; subst. destruct s; do_instrrule_triple. Qed.
+(** ** Generic OR *)
+Lemma OR_rule s (ds:DstSrc s) (v1: VWORD s) :
+   |-- specAtDstSrc ds (fun D v2 =>
+       basic (D v1 ** OSZCP?)
+             (BOP _ OP_OR ds) empOP
+             (let v := orB v1 v2 in
+              D v ** OSZCP false (msb v) (v == #0) false (lsb v))).
+Proof. do_instrrule_triple. Qed.
 
 (** We make this rule an instance of the typeclass, and leave
     unfolding things like [specAtDstSrc] to the getter tactic
     [get_instrrule_of]. *)
-Instance: forall s (r1:VReg s) (r2 : Reg) (offset : nat), instrrule (OR r1, [r2 + offset]) := fun s (r1:VReg s) r2 offset pd v1 v2 => @OR_RM_rule s pd r1 r2 v1 v2 offset _ (refl_equal _).
+Global Instance: forall s (ds : DstSrc s), instrrule (BOP s OP_OR ds) := @OR_rule.
 
-Corollary OR_RM_ruleNoFlags s (pd:DWORD) (r1:VReg s) (r2:Reg) (v1 v2:VWORD s) (offset:nat):
-  |-- basic (r1~=v1) (OR r1, [r2 + offset]) empOP (r1~=orB v1 v2)
-             @ (r2 ~= pd ** pd +# offset :-> v2 ** OSZCP?).
-Proof. autorewrite with push_at. basic apply *. Qed.
--- src/x86/instrrules/xor.v
+++ src/x86/instrrules/xor.v
@@ -2,11 +2,25 @@
 Require Import x86proved.x86.instrrules.core.
 Import x86.instrrules.core.instrruleconfig.
 
+(** ** Generic XOR *)
+Lemma XOR_rule s (ds:DstSrc s) (v1: VWORD s) :
+   |-- specAtDstSrc ds (fun D v2 =>
+       basic (D v1 ** OSZCP?)
+             (BOP _ OP_XOR ds) empOP
+             (let v := xorB v1 v2 in
+              D v ** OSZCP false (msb v) (v == #0) false (lsb v))).
+Proof. do_instrrule_triple. Qed.
+
+(** We make this rule an instance of the typeclass, and leave
+    unfolding things like [specAtDstSrc] to the getter tactic
+    [get_instrrule_of]. *)
+Global Instance: forall s (ds : DstSrc s), instrrule (BOP s OP_XOR ds) | 2 := @XOR_rule.
+
 Lemma XOR_RR_rule s (r1 r2:VReg s) v1 (v2:VWORD s):
   |-- basic (r1 ~= v1 ** r2 ~= v2 ** OSZCP?) (XOR r1, r2) empOP
             (r1 ~= (xorB v1 v2) ** r2 ~= v2 ** OSZCP false (msb (xorB v1 v2))
                             (xorB v1 v2 == #0) false (lsb (xorB v1 v2))).
-Proof. destruct s; do_instrrule_triple. Qed.
+Proof. destruct s; basic apply *. Qed.
 
 Lemma XOR_RM_rule (pd:DWORD) (r1 r2:VReg OpSize4) (v1 v2:DWORD) (offset:nat) v :
   xorB v1 v2 = v ->
@@ -14,13 +28,8 @@ Lemma XOR_RM_rule (pd:DWORD) (r1 r2:VReg OpSize4) (v1 v2:DWORD) (offset:nat) v :
             (XOR r1, [r2 + offset]) empOP
             (r1~=v ** r2 ~= pd ** pd +# offset :-> v2 **
              OSZCP false (msb v) (v == #0) false (lsb v)).
-Proof. 
-move => ?; subst. do_instrrule_triple. 
-Qed.
+Proof. move => ?; subst. basic apply *. Qed.
 
-(** We make this rule an instance of the typeclass, and leave
-    unfolding things like [specAtDstSrc] to the getter tactic
-    [get_instrrule_of]. *)
 (** We make the more specific rule have a higher priority *)
 Global Instance: forall (r1 r2 : Reg) (offset : nat), instrrule (XOR r1, [r2 + offset]) | 0
   := fun r1 r2 offset pd v1 v2 => @XOR_RM_rule pd r1 r2 v1 v2 offset _ (refl_equal _).
