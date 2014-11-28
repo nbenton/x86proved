5a0553f7b8b125f095d14de42b834ca0e85110cd
andrewjkennedy <akenn@microsoft.com>
2014-11-28 14:35:44 +0000

    Inlinealloc restored. Still issues with ssimpl etc.

--- src/x86/inlinealloc.v
+++ src/x86/inlinealloc.v
@@ -32,7 +32,7 @@ Definition allocInv :=
 *)
 Definition allocImp (n: nat) (failed: ADDR) : program :=
   MOV RDI, QWORD PTR [infoBlockReg];;
-  ADD RDI, (BOPArgI OpSize8 (n:DWORD));;
+  ADD RDI, (#n:IMM _);;
   JC  failed;;  (* A carry indicates unsigned overflow *)
   CMP QWORD PTR [infoBlockReg + 8], RDI;;
   JC  failed;;  (* A carry indicates unsigned underflow *)
@@ -53,9 +53,16 @@ Hint Unfold allocSpec : specapply.
 
 (* Perhaps put a |> on the failLabel case *)
 
+  Ltac unfoldRule R :=
+    try autounfold with specAt in R;
+    eforalls R.
+
+
 Lemma inlineAlloc_correct n failed : 
+  n < 2^n31 ->
   |-- allocSpec n failed allocInv (allocImp n failed).
 Proof.
+  move => BOUND.  
   rewrite /allocSpec/allocImp.
   specintros => *. 
   unfold_program. specintros => *.
@@ -63,11 +70,13 @@ Proof.
 
   (* MOV RDI, [infoBlockReg + 0] *)
   rewrite {3}/allocInv. specintros => infoBlock base limit. 
-  rewrite {2}/(stateIsAny RDI). specintros => oldrdi.
+  rewrite {2}/(stateIsAny RDI). specintros => oldrdi. 
 Hint Rewrite -> signExtend_fromNat : ssimpl. 
 Hint Rewrite -> addB0 : ssimpl. 
 
 Hint Unfold fromSingletonMemSpec : specapply.
+
+
 Require Import basicspectac.
 
   specapply *; first by sbazooka. 
@@ -81,26 +90,33 @@ Require Import basicspectac.
   { finish_logic. apply landL1. finish_logic. 
     rewrite /stateIsAny/allocInv. sbazooka.  }
 
- (* CMP [infoBlockReg + 8], RDI *)   
-admit.
-(*  specapply *. CMP_ruleZC. basicCMP_ZC. unfold UOPArgM4. specapply CMP_rule. 
-  specapply CMP_IndR_ZC_rule; first by rewrite /stateIsAny; sbazooka.
+ (* CMP [infoBlockReg + 8], RDI *)
+  have RULE := (CMP_ruleZC (DstSrcMR _ _ (mkMemSpec _ None (Some (infoBlockReg:BaseReg _)) None 8) RDI)).
+  specapply RULE. 
+  rewrite /stateIsAny.
+  sbazooka. (* Why doesn't this work? *)
+  rewrite sepSPC. rewrite sepSPA. reflexivity. 
+  clear RULE. 
 
   (* JC failed *)
-  specapply JC_rule; first by ssimpl.
+  specapply JC_rule. rewrite /OSZCP. sbazooka. 
 
   case LT:(ltB _ _).
-  { rewrite <-spec_reads_frame. apply limplValid. apply landL1. finish_logic. 
-    rewrite /stateIsAny/allocInv. sbazooka. }
+  { rewrite <-spec_reads_frame. apply limplValid. apply landL1. rewrite /allocInv. finish_logic_with sbazooka. 
+    reflexivity. }
 
-  (* MOV [infoBlock], EDI *)
-  specapply MOV_IndR_rule; first by ssimpl.
-  { rewrite <-spec_reads_frame. apply limplValid. apply landL2. finish_logic.
-    rewrite /allocInv/stateIsAny/natAsDWORD. sbazooka.
-
-    apply memAnySplit; last first.
-    { simpl. rewrite ltBNle /natAsDWORD in LT. rewrite -> Bool.negb_false_iff in LT. admit. } 
+  (* MOV [infoBlock], RDI *)
+  specapply *; first by ssimpl.
+  { rewrite <-spec_reads_frame. apply limplValid. apply landL2. rewrite /eval.getImm. 
+    rewrite -> (signExtend_fromNat _); last by done.
+    rewrite /allocInv/stateIsAny/natAsDWORD/eval.computeEA/eval.computeDisplacement. finish_logic. ssimpl.
+    (* sbazooka instantiates wrongly *)
+    ssplits. instantiate (1:= limit).
+    sbazooka. instantiate (1:= base). rewrite -> memAnySplit.
+    ssimpl. reflexivity. 
     { apply: addB_leB. 
-      apply injective_projections. Admitted. 
-*)
+      apply injective_projections. rewrite -> signExtend_fromNat in Hcarry; last by done. apply Hcarry. 
+      by simpl. } 
+    { rewrite ltBNle /natAsDWORD in LT. simpl. rewrite -> Bool.negb_false_iff in LT. rewrite /eval.getImm in LT. 
+      rewrite ->signExtend_fromNat in LT; last by done. apply LT. } }
 Qed.
